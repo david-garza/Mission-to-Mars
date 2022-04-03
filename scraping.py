1e2ec72f18c93bdc866d52ae73bdb2f47c6e2362
david-garza <locodave@gmail.com>
2022-04-02 22:56:37 -0500

    Finished deliverable 2

--- scraping.py
+++ scraping.py
@@ -19,7 +19,11 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+
+        # Added hemispheres dictionary by callig a function that returns 
+        # List of dictionaries that contains links and titles
+        "hemispheres": hemispheres_scrape(browser)
     }
 
     browser.quit()
@@ -91,6 +95,57 @@ def mars_facts():
     df.set_index('description', inplace=True)
     
     return df.to_html()
+
+# New function that scrapes images and tiles for hemisphers of Mars
+# Returns a list of dictionaries
+# Copied from the end of Mission_to_Mars_Challenge.py
+# Passing the browser object to the function is required for browser to work
+
+def hemispheres_scrape(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles. (Note, will be a list of dictionaries)
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+
+    # Convert the webpage into an HTML object and then parse it using soup
+    hemi_html = browser.html
+    hemi_parsed = soup(hemi_html, "html.parser")
+
+    #3 continued...
+
+    # Find each hemishphere section with its hyper link tot he full image.
+    hemi_loc_list = hemi_parsed.find_all("div", class_="description")
+
+    # Create a loop that goes through each of the sections, finds the link to next page
+    # goes to the next page, finds the jpg address and description, and adds it to the image list
+    for hemi in hemi_loc_list:
+
+        # Set the address of the new site 
+        hemi_address = url + hemi.find("a")["href"]
+
+        # Send the browerse to the new site
+        browser.visit(hemi_address)
+
+        # Parse the new site
+        html = browser.html
+        img_des = soup(html, "html.parser")
+
+        # Find the image address for current hemispher jpg
+        img_url = url + img_des.find("div",class_="downloads").find("a")["href"]
+
+        # Find the title
+        title = img_des.find("h2",class_="title").text
+
+        # add to the list
+        hemisphere_image_urls.append({"img_url": img_url, "title":title})
+
+    # Modified to return a list of dictionaries to the calling stack
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
     # If running as script, print scraped data
